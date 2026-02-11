eaeb6436a3e3262f8a237a0aee614b0f7017b538
Jon Chui <jon@atlas3i.com>
2026-02-11 23:45:26 +0800

    Version bump to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HarfbuzzEx.MixProject do
   def project do
     [
       app: :harfbuzz_ex,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps()

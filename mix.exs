75e8f1c696e4ea5918bb2531d96c2c1b3e0b1404
Jon Chui <jon@atlas3i.com>
2026-02-12 00:08:33 +0800

    remove building for old platforms

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HarfbuzzEx.MixProject do
   def project do
     [
       app: :harfbuzz_ex,
-      version: "0.2.2",
+      version: "0.2.5",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps()

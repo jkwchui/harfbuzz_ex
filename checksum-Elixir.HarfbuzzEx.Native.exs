4c70bb64c2c28c31fdf192ba1cc096adaf72254d
Jon Chui <jon@atlas3i.com>
2026-04-17 18:26:10 +0800

    add 1.1.0 checksums

---
+++ checksum-Elixir.HarfbuzzEx.Native.exs
@@ -0,0 +1,10 @@
+%{
+  "harfbuzz_ex-v1.1.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:3426706e858bba4169310fe9a802f510ae88a2ce14304ae7ca63d76d8770d746",
+  "harfbuzz_ex-v1.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:f9c69643950b2dda697199d1f0b133a758ed8d469711b686de454119b024e9a3",
+  "harfbuzz_ex-v1.1.0-nif-2.16-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:bdb6d32771dcc9dae6d3ae51749bd438b08f705cc862d0bdd1f81f9f7f8c2706",
+  "harfbuzz_ex-v1.1.0-nif-2.16-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:a6e6701f09173a28494bbb1210cfcd9ec15f2de976c35ed0d47cd3a57af13b73",
+  "libharfbuzz_ex-v1.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:2e87f70d542494ecdfca657e23b45ef01f8bc15189b930265e1cfef582f9f783",
+  "libharfbuzz_ex-v1.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:d2ff4c9e3582bb4acd6784442ceb7616dd3ce513d436491de202c6720db3525a",
+  "libharfbuzz_ex-v1.1.0-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:7518ac46ae1cefc1773c6088383f4e9a6e6ad7ea31d9762e8726e5f7e539f6b4",
+  "libharfbuzz_ex-v1.1.0-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:4ef2ee8f1ad3911438589a0aa7bcd74d7abd8fda8d18aa97e81af01a4738cb67",
+}
